bae5a3bc84fd315e1708c9f60a9dc53b6807e541
Santhosh T <santhosh25995@gmail.com>
2026-07-29 20:49:44 +0530

    Week 1 Day 5 - Created Initial Hospital Database Tables
    • Created Patients table SQL script
    • Created Doctors table SQL script
    • Added database table creation report
    • Updated Development Log
    • Updated Files Created Log
    • Added documentation for initial database design

---
+++ sql/01_create_patients_doctors_tables.sql
@@ -0,0 +1,29 @@
+-- =====================================
+-- SMART HEALTHCARE PLATFORM
+-- Patients & Doctors Tables
+-- Week 1 - Day 5
+-- =====================================
+
+CREATE TABLE Patients (
+    patient_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    gender VARCHAR(10),
+    age INT,
+    phone VARCHAR(20),
+    email VARCHAR(100),
+    blood_group VARCHAR(10),
+    address TEXT,
+    registration_date DATE
+);
+
+CREATE TABLE Doctors (
+    doctor_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    specialization VARCHAR(100),
+    phone VARCHAR(20),
+    email VARCHAR(100),
+    experience INT,
+    department VARCHAR(100)
+);
